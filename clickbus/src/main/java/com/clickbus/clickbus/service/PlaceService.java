3bf6f48d963cd97c2635c9a6975821cbb8fb4835
RuanCrysthian <ruanlima155@gmail.com>
2023-08-02 15:46:13 -0300

    feat: create the Service to Place

---
+++ clickbus/src/main/java/com/clickbus/clickbus/service/PlaceService.java
@@ -0,0 +1,42 @@
+package com.clickbus.clickbus.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clickbus.clickbus.model.Place;
+import com.clickbus.clickbus.repository.PlaceRepository;
+
+@Service
+public class PlaceService {
+    @Autowired
+    PlaceRepository repository;
+
+    public Place create(Place place) {
+        return repository.save(place);
+    }
+
+    public List<Place> findAll() {
+        return repository.findAll();
+    }
+
+    public Place findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Place not found!"));
+    }
+
+    public Place update(Place place) {
+        Place entity = repository.findById(place.getId()).orElseThrow(() -> new RuntimeException("Place not Found!"));
+        entity.setName(place.getName());
+        entity.setSlug(place.getSlug());
+        entity.setCity(place.getCity());
+        entity.setState(place.getState());
+        return repository.save(entity);
+    }
+
+    public void delete(Long id) {
+        Place entity = repository.findById(id).orElseThrow(() -> new RuntimeException("Place not Found!"));
+        repository.delete(entity);
+    }
+
+}
