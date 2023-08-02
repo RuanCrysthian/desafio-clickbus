606b47ea416883d4b01a472a269374f76104e8c4
RuanCrysthian <ruanlima155@gmail.com>
2023-08-02 11:18:33 -0300

    feat: create the model of Place

---
+++ clickbus/src/main/java/com/clickbus/clickbus/model/Place.java
@@ -0,0 +1,116 @@
+package com.clickbus.clickbus.model;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "place")
+public class Place implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 80)
+    private String name;
+
+    @Column(name = "slug", nullable = false, length = 80)
+    private String slug;
+
+    @Column(name = "city", nullable = false, length = 80)
+    private String city;
+
+    @Column(name = "state", nullable = false, length = 80)
+    private String state;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, length = 80)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false, length = 80)
+    private LocalDateTime updatedAt;
+
+    public Place() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Place)) {
+            return false;
+        }
+        Place place = (Place) o;
+        return Objects.equals(id, place.id) && Objects.equals(name, place.name) && Objects.equals(slug, place.slug)
+                && Objects.equals(city, place.city) && Objects.equals(state, place.state)
+                && Objects.equals(createdAt, place.createdAt) && Objects.equals(updatedAt, place.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, slug, city, state, createdAt, updatedAt);
+    }
+
+}
